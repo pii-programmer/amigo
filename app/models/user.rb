3363454966215e65d9502e6f2dd89ed3117f6c26
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-05-31 12:59:59 +0900

    devise導入/devise_userモデルバリデーション・アソシエーション/devise_userテーブルカラム追加/chatモデルアソシエーション追記

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX
+
+         validates :nickname, presence: true, length: { maximum: 30 }
+         
+         belongs_to :chat
+end
