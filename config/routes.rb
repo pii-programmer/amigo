6a82be47a5b304937ae2dca2e215416c72e7a7b7
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-06-02 13:23:40 +0900

    roomモデル/マイグレート/コントローラー/ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
       get 'introduce'
     end
   end
+  resources :rooms, only: [:new, :create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_064725) do
+ActiveRecord::Schema.define(version: 2021_06_02_035412) do
 
   create_table "chats", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content", null: false
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2021_05_31_064725) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_chats_on_user_id"
+  end
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
