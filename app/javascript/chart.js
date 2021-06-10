cce072dd34f543c03dd448803e0df105a8abb172
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-06-10 21:57:56 +0900

    アミーゴ指標完成

--- app/javascript/chart.js
+++ app/javascript/chart.js
@@ -1,28 +1,16 @@
 function chart(){
   const data = {
-    labels: ["1", "2", "3", "4", "5", "6", "7"],
+    labels: ["amigo"],
     datasets: [{
       axis: 'y',
-      label: 'My First Dataset',
-      data: [65, 59, 80, 81, 56, 55, 40],
+      label: 'アミーゴ指標',
+      data: [85],
       fill: false,
       backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(255, 205, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(201, 203, 207, 0.2)'
+        'rgba(255, 99, 132, 0.2)'
       ],
       borderColor: [
-        'rgb(255, 99, 132)',
-        'rgb(255, 159, 64)',
-        'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-        'rgb(54, 162, 235)',
-        'rgb(153, 102, 255)',
-        'rgb(201, 203, 207)'
+        'rgb(255, 99, 132)'
       ],
       borderWidth: 1
     }]
