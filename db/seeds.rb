edc8b822b0a21300a389c8081ad08130ddae9073
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-28 15:30:00 +0900

    seedsファイルにactiverecord-import

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = []
+10.times do |i|
+  users << User.new(level: i+1)
+end
+User.import users
+
+levels = []
+100.times do |l|
+  level =+ l+1
+  levels << Level.new(level: l+1)
+end
+Level.import levels
