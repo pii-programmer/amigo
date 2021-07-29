18b21e1dd4b589bc4a6390503889f658cc93c3dc
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-30 08:55:42 +0900

    【マイグレーションファイル】levelsテーブルにuser_id紐づける

--- db/migrate/20210728064058_create_levels.rb
+++ db/migrate/20210728064058_create_levels.rb
@@ -1,7 +1,8 @@
 class CreateLevels < ActiveRecord::Migration[6.0]
   def change
     create_table :levels do |t|
-      t.integer  :level, null: false
+      t.integer      :level, null: false
+      t.references   :user,  null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -43,8 +43,10 @@ ActiveRecord::Schema.define(version: 2021_07_28_064058) do
 
   create_table "levels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "level", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_levels_on_user_id"
   end
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
